00f2070af0a44c12b152a8d4796e3db90fdf40f9
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-03-23 15:54:55 +0900

    코드 주석 추가

--- C_Algorithm/Array.c
+++ C_Algorithm/Array.c
@@ -3,12 +3,16 @@
 int main(void) {
 	int a[5][7];
 	int i, j, sum = 3;
+
+	// 배열 초기화
 	for (int i = 0; i < 4; i++) {
 		a[i][6] = 0;
 	}
 	for (int i = 0; i < 6; i++) {
 		a[4][i] = 0;
 	}
+
+	//이중 for문으로 값 대입
 	for (i = 0; i < 4; i++) {
 		for (j = 0; j < 6; j++) {
 			a[i][j] = sum;
@@ -18,6 +22,7 @@ int main(void) {
 		}
 	}
 
+	// 이중 for문으로 출력
 	for (i = 0; i < 5; i++) {
 		for (j = 0; j < 7; j++) {
 			if (i == 4 && j == 6) break;
