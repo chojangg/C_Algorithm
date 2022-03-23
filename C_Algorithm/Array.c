d4be88449d21ca1fb425ff47e1d9c234fa8bd9e1
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-03-23 15:53:36 +0900

    초기 커밋.

---
+++ C_Algorithm/Array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+	int a[5][7];
+	int i, j, sum = 3;
+	for (int i = 0; i < 4; i++) {
+		a[i][6] = 0;
+	}
+	for (int i = 0; i < 6; i++) {
+		a[4][i] = 0;
+	}
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 6; j++) {
+			a[i][j] = sum;
+			a[i][6] += sum;
+			a[4][j] += sum;
+			sum += 3;
+		}
+	}
+
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < 7; j++) {
+			if (i == 4 && j == 6) break;
+			printf("%3d ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
