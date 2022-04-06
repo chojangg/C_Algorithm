e6cf956e32bf911c091295ac663e25faac4ce13c
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-04-06 15:22:20 +0900

    scanf_s -> scanf

--- C_Algorithm/variable.c
+++ C_Algorithm/variable.c
@@ -10,27 +10,27 @@ int main(void) {
 	double f2;
 
 	printf("첫 번째로 short 정수 입력: ");
-	scanf_s("hd", &sh1);
+	scanf("hd", &sh1);
 	getchar();
 	printf("두 번째로 int 정수 - 정수 - 정수 입력: ");
-	scanf_s("%d - %d - %d", &d1, &d2, &d3);
+	scanf("%d - %d - %d", &d1, &d2, &d3);
 	getchar();
 	printf("세 번째로 문자, 문자 입력: ");
-	scanf_s("%c, %c", &ch1, 1, &ch2, 1);
+	scanf("%c, %c", &ch1, 1, &ch2, 1);
 	getchar();
 	printf("네 번째로 한 문자 입력: ");
 	ch3 = getchar();
 	printf("다섯 번째로 8진수, 16진수 입력: ");
-	scanf_s("%o, %x", &d4, &d5);
+	scanf("%o, %x", &d4, &d5);
 	getchar();
 	printf("여섯 번째로 long 정수 입력: ");
-	scanf_s("%ld", &l1);
+	scanf("%ld", &l1);
 	printf("일곱 번째로 long long 정수 입력: ");
-	scanf_s("%lld", &l2);
+	scanf("%lld", &l2);
 	printf("여덟 번째로 float 실수 입력: ");
-	scanf_s("%f", &f1);
+	scanf("%f", &f1);
 	printf("아홉 번째로 double 실수 입력: ");
-	scanf_s("%lf", &f2);
+	scanf("%lf", &f2);
 
 	printf("\n\n출 력\n");
 	printf("sh1=%hd, d1=%d, d2=%d, d3=%d\n", sh1, d1, d2, d3);
